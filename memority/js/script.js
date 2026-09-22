5561085a676c034bec58f6ba44d8e2031befc972
Kylian Langlois <kylian.langlois@etu.unicaen.fr>
2026-09-22 14:29:35 +0200

    Listed every Verity variant in script.js

--- memority/js/script.js
+++ memority/js/script.js
@@ -1,3 +1,11 @@
 let dimension = 150;
-let imgStart = Math.floor(Math.random() * (9 - 1 + 1));
 
+const filenames = [
+    'Cruelty','Curiosity','Falsity',
+    'Lovity','Moggity','MonsterVerity',
+    'Obesity','Verity','VerityCreepyOpen',
+    'VerityDispleased'
+];
+const images = filenames.map(name => `./images/${name}.webp`);
+
+console.log(images);
