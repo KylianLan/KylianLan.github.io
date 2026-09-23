2e5e1a8e21b2cfc4878eb21dad3a7186ed1a49fc
Kylian Langlois <kylian.langlois@etu.unicaen.fr>
2026-09-23 08:18:58 +0200

    Added reset button

---
+++ memority/js/script.js
@@ -0,0 +1,28 @@
+const game = document.getElementById('game');
+const resetButton = document.getElementById('reset-game');
+
+const optionnalText = document.getElementById('opt');
+
+let dimension = 150;
+
+const filenames = [
+    'Cruelty','Curiosity','Falsity',
+    'Lovity','Moggity','MonsterVerity',
+    'Obesity','Verity','VerityCreepyOpen',
+    'VerityDispleased'
+];
+const images = filenames.map(name => `./images/${name}.webp`);
+let cards = [...images, ...images];
+
+
+function shuffle(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+    return cards;
+}
+
+function initGame() {
+    cards = shuffle(cards);
+}
